a297fc3b88a0d52d348d23728343806f823f5bdf
Michael Ashby & Arturo Perez <pair+ashbymichael.vrturo@devbootcamp.com>
2015-08-24 12:20:33 -0700

    lunch

--- todo_list.js
+++ todo_list.js
@@ -1,10 +1,23 @@
-var newTodoList = function() {
-	// ???
+
+var TodoList = function() {
+  this.tasks = []
+    this.add = function(item){
+      var task = {}
+        task.description = item;
+        task.completed = false;
+      this.tasks.push(task)
+    };
+    this.list = function(){
+      for (i = 0; i < this.tasks.length; i++) {
+       console.log(this.tasks[i]);
+      }
+    }
+
 };
 
 
-
-// Driver code
-
-
-var todoList = newTodoList();
+var groceryList = new TodoList();
+groceryList.add('bread');
+groceryList.add('cheese');
+groceryList.add('milk');
+groceryList.tasks[0]
